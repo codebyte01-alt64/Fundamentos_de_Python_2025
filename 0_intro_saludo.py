9a1c2a6c495b18613b0acb139e988a16617c2634
codebyte01-alt64 <codebyte01@outlook.com>
2025-05-06 22:56:14 -0500

    "Introduccion a Python. Saludos y Tipos de datos"

---
+++ 0_intro_saludo.py
@@ -0,0 +1,31 @@
+# Esto es un comentario o sea que no se ejecuta
+# Esto es un comentario "Hola mundo!'
+print("Hola Mundo!") # imprime "Hola Mundo!" en la consola despues de guardar el archivo
+print("Hola Python!") # imprime "Hola Python!" en la consola
+print('Hola Python!') # Esto es una cadena de texto
+
+
+# Consultar el tipo de dato
+
+print(type("Hola Python!")) # Tipo de dato str (string o cadena de texto)
+print(type(5)) # Tipo de dato int (entero)
+print(type(5.0)) # Tipo de dato float (decimal)
+print(type(5 + 2j)) # Tipo de dato complex (complejo)
+print(type(True)) # Tipo de dato bool (booleano)
+
+"""
+
+Esto es
+un comentario 
+de varias lineas
+
+"""
+
+'''
+
+Esto tambien es
+un comentario 
+de varias lineas
+
+'''
+
